75186f92d924273c10bedb8c66498be4a0cb5f1d
Maciek Piwowarczyk <maciejpiwowarczyk@gmail.com>
2018-11-17 23:09:04 +0100

    initial

---
+++ RxPlayground/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace RxPlayground 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            var xs = Observable.Range(1, 10);
+            xs.Subscribe(x => Console.WriteLine("Received {0}", x),
+                () => Console.WriteLine("Done"));
+        }
+    }
+}
