40c91f884e6db80244b23d0bc8a2d60d0ee34903
Maciek Piwowarczyk <maciejpiwowarczyk@gmail.com>
2018-11-18 18:46:01 +0100

    rozwiazanie przez scheduler

--- RxPlayground/Printer.cs
+++ RxPlayground/Printer.cs
@@ -7,9 +7,15 @@ namespace RxPlayground
     static class Printer
     {
         public static void Print(string message, [CallerMemberName] string memberName = "")
-        {
+        {            
+            var threadDesc = Thread.CurrentThread.ManagedThreadId.ToString();
+            var syncContext = SynchronizationContext.Current;            
+            if (syncContext != null) 
+            {
+                threadDesc += $" {syncContext}";
+            }
             var dt = DateTime.Now.ToString("HH:mm:ss,fff");
-            Console.WriteLine($"{dt} [{Thread.CurrentThread.ManagedThreadId}] {memberName}: " + message);
+            Console.WriteLine($"{dt} [{threadDesc}] {memberName}: " + message);
         }
     }
 }
--- RxPlayground/Program.cs
+++ RxPlayground/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Threading;
 
@@ -8,48 +9,49 @@ namespace RxPlayground
     {
         static void Main(string[] args)
         {
-            var ts1 = Observable.Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(1000))
-                .Select(i => $"ts1-{i}-{Guid.NewGuid()}");
-            var ts2 = Observable.Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(1500))
-                .Select(i => $"ts2-{i}");
+            // var ts1 = Observable.Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(1000))
+            //     .Select(i => $"ts1-{i}-{Guid.NewGuid()}");
+            // var ts2 = Observable.Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(1500))
+            //     .Select(i => $"ts2-{i}");
 
-            var hotTimer = new HotTimer(TimeSpan.FromMilliseconds(1000));
             // var evts = ts1 //.Merge(ts2);
             //     .Publish(); //connectable
+
+            var hotTimer = new HotTimer(TimeSpan.FromMilliseconds(1000));
             var evts = hotTimer.GetObservable();
 
-            //evts.Subscribe(ReceiveShort);
-            //TODO: tutaj jakas magia, ktora sprawi, ze publikowanie bedzie czekalo
-            //na zakonczenie ReceiveLong i zachowywalo kolejnosc
-            evts.Subscribe(ReceiveLong, () => {
-                Printer.Print($"Completed, handled: {receiveLongHandled} events");
-            });
-            //TODO: wspomniana wyzej magia nie powinna wplywac na to
-            //obecnie i tak to nie dziala poprawnie
-            evts.Subscribe(ReceiveShort);
+            evts
+                .ObserveOn(NewThreadScheduler.Default) //dedykowany watek dla obslugi tego observera
+                .Subscribe(ReceiveLong, () =>
+                {
+                    Printer.Print($"Completed-ReceiveLong, handled: {receiveLongHandled} events");
+                });
+
+            evts
+                .Subscribe(ReceiveShort, () =>
+                {
+                    Printer.Print($"Completed-ReceiveShort, handled: {receiveShortHandled} events");
+                });
 
             Console.WriteLine("Press any key...");
             Console.Read();
         }
 
+        private static int receiveShortHandled = 0;
         static void ReceiveShort(string evtDesc)
         {
             Printer.Print($"Got {evtDesc}");
+            receiveShortHandled++;
         }
 
         private static object receiveLongLock = new object();
         private static int receiveLongHandled = 0;
         static void ReceiveLong(string evtDesc)
         {
-            //TODO: nie chcialbym locka - chcialbym miec to zalatwione przez rx i nie miec popsutej kolejnosci
-            lock(receiveLongLock)
-            {
-                Printer.Print($"Starting {evtDesc}");
-                Thread.Sleep(TimeSpan.FromMilliseconds(2000));
-                Printer.Print($"Finished {evtDesc}");
-                receiveLongHandled++;
-                Printer.Print($"Handled {receiveLongHandled}");
-            }
+            Printer.Print($"Starting {evtDesc}");
+            Thread.Sleep(TimeSpan.FromMilliseconds(2000));
+            Printer.Print($"Finished {evtDesc}");
+            receiveLongHandled++;
         }
     }
 }
