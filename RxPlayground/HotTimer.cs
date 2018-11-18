dfe9e59d716d92f017d8ee42f419ff9813b2446f
Maciek Piwowarczyk <maciejpiwowarczyk@gmail.com>
2018-11-18 22:23:48 +0100

    bez subjectow

--- RxPlayground/HotTimer.cs
+++ RxPlayground/HotTimer.cs
@@ -1,40 +1,57 @@
 using System;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
 using System.Timers;
 
 namespace RxPlayground
 {
     class HotTimer
-    {        
+    {
+        class HotTimerEventArgs : EventArgs
+        {
+            public string EventId { get; private set; }
+            public HotTimerEventArgs(string eventId)
+            {
+                EventId = eventId;
+            }
+        }
+
         private int eventNumber = 0;
-        private readonly Subject<string> subject = new Subject<string>();        
-        private readonly Timer timer; 
+        private event EventHandler<HotTimerEventArgs> OnElapsedEvent;
+        private readonly Timer timer;
 
         public HotTimer(TimeSpan interval)
-        {            
+        {
             this.timer = new Timer(interval.TotalMilliseconds);
             this.timer.Elapsed += OnElapsed;
         }
 
+        HotTimerEventArgs GetNextHotTimerEventArgs()
+        {
+            var eventId = $"hottimer-{eventNumber}-{Guid.NewGuid()}";
+            return new HotTimerEventArgs(eventId);
+        }
+
         private void OnElapsed(object sender, ElapsedEventArgs e)
-        {            
+        {
             eventNumber++;
             if (eventNumber > 10)
             {
-                subject.OnCompleted();
                 timer.Enabled = false;
                 return;
             }
-            var eventId = $"hottimer-{eventNumber}-{Guid.NewGuid()}";            
-            Printer.Print($"Publishing event: {eventId}");
-            subject.OnNext(eventId);            
+            var eventArgs = GetNextHotTimerEventArgs();
+            Printer.Print($"Publishing event: {eventArgs.EventId}");
+            OnElapsedEvent(this, eventArgs);
         }
 
         public IObservable<string> GetObservable()
         {
+            var obs = Observable.FromEventPattern<HotTimerEventArgs>(
+                h => OnElapsedEvent += h,
+                h => OnElapsedEvent -= h
+            ).Select(ev => ev.EventArgs.EventId);
             timer.Enabled = true;
-            return subject.AsObservable();
+            return obs;
         }
     }
 }
